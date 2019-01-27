1bf03818dc00a43abb83b4b889f92d19d3dd7e21
PoojhaM <47033098+PoojhaM@users.noreply.github.com>
2019-01-27 11:54:11 +0530

    Create set1.7.c

---
+++ set1.7.c
@@ -0,0 +1,18 @@
+#include<sdtio.h>
+#include<conio.h>
+void main()
+{
+int t,n;
+clrscr();
+scanf("%d",&t);
+while(t>0)
+{
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("hello");
+}
+t--;
+}
+getch();
+}
