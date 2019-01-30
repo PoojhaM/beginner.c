6b2f532ed664294d6ab4be8e08b218935b87b5ee
PoojhaM <47033098+PoojhaM@users.noreply.github.com>
2019-01-30 19:58:32 +0530

    Rename set4.31.c to set4.32.c

---
+++ set4.32.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[20];
+int x,count=1;
+clrscr();
+gets(a);
+for(x=0;x<n;x++)
+{
+if(a[x]==' ')
+{
+count++;
+}
+}
+printf("%d",count);
+getch();
+}
