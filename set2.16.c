89110e92cc37cce86916ec773a0fdbafa579f6c9
PoojhaM <47033098+PoojhaM@users.noreply.github.com>
2019-01-30 06:02:43 +0530

    Create set2.16.c

---
+++ set2.16.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,y,i,j=2;
+clrscr();
+scanf("%d%d",&x,&by);
+for(i=a;i<b-1;i++)
+{
+if(i%j!=0)        //
+{
+printf("\nprime:%d",i);
+}
+}
+getch();
