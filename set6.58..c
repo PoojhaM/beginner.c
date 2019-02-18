d07a3849f71ba2cb2339168219a04dc27853d29f
PoojhaM <47033098+PoojhaM@users.noreply.github.com>
2019-02-18 10:45:12 +0530

    Create set6.58..c

---
+++ set6.58..c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#iclude<conio.h>
+void main()
+{
+int n,k,a[10],i,c=0;
+clrscr();
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+if(a[i]==k)
+{
+c++;
+break;
+}
+}
+if(c==1)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
