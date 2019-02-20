8237792866e2eb192c019cca85e6c468d5756c2b
PoojhaM <47033098+PoojhaM@users.noreply.github.com>
2019-02-20 19:02:12 +0530

    Update set10.97.c

--- set10.97.c
+++ set10.97.c
@@ -2,15 +2,15 @@
 #include<conio.h>
 void main()
 {
-int n,r,s=0;
+int n,r,s1=0;
 clrscr();
 scanf("%d",&n);
 while(n)
 {
 r=n%10;
-s=s*10+r;
+s1=s1*10+r;
 n=n/10;
 }
-printf("%d",s);
+printf("%d",s1);
 getch();
 }
